12994f7edea28bc30be39f9c0be8b5d5584ae210
18696341302 <18696341302@163.com>
2021-12-17 17:58:41 +0800

    增加环境变量可以访问线上地址

--- vite.config.ts
+++ vite.config.ts
@@ -7,57 +7,64 @@
  * @FilePath: \musicwangyi\vite.config.ts
  * 版权声明
  */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import ViteComponents, { VantResolver } from 'vite-plugin-components'  // 自动的注册vant组件 
 import styleImport from 'vite-plugin-style-import'
 import { svgBuilder } from './src/utils/svgBuilder'
 import viteSvgIcons from 'vite-plugin-svg-icons'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // svgBuilder('./src/assets/icon/svg/'),
-    viteSvgIcons({
-      iconDirs: [resolve(process.cwd(), 'src/assets/icon/svg')],
-      symbolId: 'icon-[name]'
-    }),
-    ViteComponents({
-      customComponentResolvers: [VantResolver()],
-    }),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'vant',
-          esModule: true,
-          resolveStyle: (name) => `vant/es/${name}/style`
+//@ts-ignore
+export default defineConfig(({ mode }) => {
+  // 获取环境变量
+  const httpurl = loadEnv(mode, process.cwd()).VITE_BASE_API
+
+  return {
+    plugins: [
+      vue(),
+      // svgBuilder('./src/assets/icon/svg/'),
+      viteSvgIcons({
+        iconDirs: [resolve(process.cwd(), 'src/assets/icon/svg')],
+        symbolId: 'icon-[name]'
+      }),
+      ViteComponents({
+        customComponentResolvers: [VantResolver()],
+      }),
+      styleImport({
+        libs: [
+          {
+            libraryName: 'vant',
+            esModule: true,
+            resolveStyle: (name) => `vant/es/${name}/style`
+          }
+        ]
+      })
+    ],
+    server: {
+      host: true,
+      port: 1102,
+      proxy: {
+        '/api': {
+          target: mode === 'online' ? httpurl : 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
-      ]
-    })
-  ],
-  server: {
-    host: true,
-    port: 1102,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      open: true
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        }
       }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-})
+    },
+  }
+}
+)
